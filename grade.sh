c988fcb10d71a9e1680b9dbb489a312f45f920bd
Merlin Ho <45127865+clairvoyage@users.noreply.github.com>
2024-05-28 12:46:07 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -8,9 +8,39 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ -f student-submission/ListExamples.java ]]
+then
+  echo "ListExamples.java file found."
+else
+    echo "ListExamples.java file not found"
+    echo "Grade: 0"
+    exit
+fi
 
+mv student-submission/ListExamples.java grading-area
+cp -r TestListExamples.java grading-area
+cp -r lib grading-area
+cd grading-area || exit
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java &> compliation-results.txt
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > test-results.txt
+error=$?
+
+cd .. || exit
+if [[ error -ne 0 ]]
+then
+  echo "Error code " $error
+else
+  echo "All tests passed"
+  exit
+fi
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+test_failures=$(tail -4 grading-area/test-results.txt | grep Failures | cut -d " " -f 6)
+compilation_errors=$(tail -2 grading-area/compliation-results.txt | grep error | cut -d " " -f 1 )
+
+score=$((100 - test_failures * 10 - compilation_errors * 10))
+echo "Score: $score"
